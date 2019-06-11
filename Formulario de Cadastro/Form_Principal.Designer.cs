75441f6c256c67388b1b1d50060dadb503479d58
Matheus Ferreira <matheusfernandofreire@hotmail.com>
2019-06-11 16:38:40 -0300

    Começo da tela Principal.

--- Formulario de Cadastro/Form_Principal.Designer.cs
+++ Formulario de Cadastro/Form_Principal.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Formulario_de_Cadastro
 {
-    partial class Form1
+    partial class Form_Principal
     {
         /// <summary>
         /// Variável de designer necessária.
@@ -28,10 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Form_Principal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(836, 529);
+            this.Name = "Form_Principal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Perfil Sócio Econômico";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
